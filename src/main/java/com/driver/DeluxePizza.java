7ec69612e14d853a888f1222c2af5e70ed0e79d9
Jawad TP <jawadtp248@gmail.com>
2022-10-21 18:40:48 +0530

    check

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,12 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        super.addExtraCheese();
+        super.addExtraToppings();
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,8 +3,14 @@ package com.driver;
 public class Pizza {
 
     private int price;
+    private int total;
     private Boolean isVeg;
+
+    private boolean isCheeseaAdd=false;
+    private boolean isToppingAdd=false;
+    private boolean isTake=false;
     private String bill;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -12,22 +18,62 @@ public class Pizza {
     }
 
     public int getPrice(){
+        if(isVeg){
+            this.price=300;
+        }
+        else {
+            this.price=400;
+        }
         return this.price;
     }
 
     public void addExtraCheese(){
+
+        isCheeseaAdd=true;
         // your code goes here
     }
 
     public void addExtraToppings(){
+       isToppingAdd=true;
         // your code goes here
     }
 
     public void addTakeaway(){
+        isTake=true;
         // your code goes here
     }
 
     public String getBill(){
+        if(isVeg){
+            this.total=300;
+        }
+        else {
+            this.total=400;
+        }
+        bill="Base Price Of The Pizza: "+this.total+"\n";
+
+        if(isCheeseaAdd){
+            bill+= "Extra Cheese Added: "+80+"\n";
+            this.total+=80;
+        }
+        if(isToppingAdd){
+            if(isVeg){
+                bill+= "Extra Toppings Added: "+70+"\n";
+                this.total+=70;
+            }
+            else{
+                bill+= "Extra Toppings Added: "+120+"\n";
+                this.total+=120;
+            }
+        }
+        if(isTake){
+            bill+= "Paperbag Added: "+20+"\n";
+            this.total+=20;
+        }
+
+        bill+="Total Price: "+total;
+
+
         // your code goes here
         return this.bill;
     }
